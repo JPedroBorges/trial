833c1d5422f420ab7bca5c772eb65bef684967cb
DESKTOP-N38M0RU\user <joao.pedro.borges@hotmail.com>
2017-04-13 16:42:56 +0100

    return temperature depending on selected county

---
+++ public/js/client.js
@@ -0,0 +1,29 @@
+$(function(){
+
+	$.get('/counties', appendToList);
+
+	function appendToList(counties){
+		var list = ['<option value="vazio"></option>'];
+		for(var i in counties){
+			list.push($('<option>',{value: counties[i], text: counties[i]}));
+		}
+		$('.counties-sel').append(list);
+		$('.infocenas').hide();
+	}
+
+	$(function() {
+	    $('.counties-sel').change(function() {
+	         $('.county-name').html($(this).val());
+			 $.get('/weather/' + $(this).val() + '/daily', setTemperature);
+	    }).change();
+	});
+
+
+	function setTemperature(temperature){
+		$('.counties-sel').val() === 'vazio' ? $('.infocenas').hide() : $('.infocenas').show();
+		$('.temperature').html(temperature + 'º');
+
+
+
+	}
+});
