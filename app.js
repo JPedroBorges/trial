9702985ca0b8cf962e35b72514ed0aefab03cfcf
DESKTOP-N38M0RU\user <joao.pedro.borges@hotmail.com>
2017-04-13 14:19:11 +0100

    nodejs redicts browser to html page; static front end

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var path = require('path');
+
 var app = express();
+
 var logger = require('./logger');
 var counties = require('./routes/counties');
 var weather = require('./routes/weather');
@@ -8,8 +11,9 @@ app.use(logger);
 app.use('/counties', counties);
 app.use('/weather', weather);
 
+app.use("/", express.static(path.join(__dirname, 'public')));
 app.get('/', function(request, response){
-	response.status(200).json("Welcome to our useful API");
+	response.sendFile(__dirname + '/public/index.html');
 });
 
 app.listen(3000, function(){
