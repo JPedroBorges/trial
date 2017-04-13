950abdee1d150122e99b4f9d8ee1e9442723c918
DESKTOP-N38M0RU\user <joao.pedro.borges@hotmail.com>
2017-04-13 11:01:05 +0100

    create routes

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
+var logger = require('./logger');
+var weather = require('./routes/weather');
+
+app.use(logger);
+app.use('/weather', weather);
 
 app.get('/', function(request, response){
-	response.status(201).json("Welcome to our useful API");
+	response.status(200).json("Welcome to our useful API");
 });
 
 app.listen(3000, function(){
---
+++ routes/weather.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var request = require("request");
+var router = express.Router();
+
+
+module.exports = router;
